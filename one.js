4c50dee2495ef3228e53fe46869b5db23e094b54
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-01-11 14:44:53 +0530

    Create one.js

---
+++ one.js
@@ -0,0 +1,83 @@
+console.log("Hello world!");
+console.log("welcome future software developer");
+name = "Tony Stark";
+console.log(name);
+age = 32;
+console.log(age);
+ x = null ;
+ console.log(x);
+ y = undefined ;//pta hi nhi iske andar kya hai -> undefinded
+ radius = 2;
+area = 22/7 * radius * radius ;
+console.log(area);
+
+_hello = "hello brother";
+console.log(_hello);
+hel = false;
+console.log(hel);
+Console = "hey";
+console.log(Console);
+fullName = "Rishabh Kushwaha";
+console.log(fullName);
+isFollow = "yeah";
+console.log(isFollow);
+let p = "tony stark";
+console.log(p);
+const price = "$5555";
+console.log(price);
+var helloo = 44;
+console.log(helloo);
+let a = 99;
+a = 00;
+a =55;
+console.log(a);
+const b = 90;
+// b = 88; we can't use 
+console.log(b);
+let k ;
+k = "waht";
+console.log(k);
+{
+          let apple = 5;
+          console.log(apple);
+}
+let apple = 44;
+console.log(apple);
+typeof(apple);
+console.log(typeof(apple));
+console.log(typeof(price));
+let xa;
+console.log(typeof(xa));
+let c = BigInt("343434");
+let h = Symbol("&");
+console.log(typeof(c));
+console.log(h);
+const student = {
+           year : 4545,
+           CGPZ : 9.9,
+};
+console.log(student);
+console.log(student.year);
+student["year"]  = student["year"] + 1;
+console.log(student["year"]);
+
+const product = {
+          nameOfProduct : "CT ball pen" , 
+          ratingOfProduct : 4.1,
+          mrp : "$21" ,
+          discount : "5%",
+          isDeal : false , 
+};
+
+console.log(product);
+console.log(5+2);
+console.log("34"+"54");
+
+const profile = {
+          userName : "rrisavv" ,
+          posts : 3 ,
+          followers : 100 ,
+          following : 77 ,
+          bio : "Nirdosh Kushwaha" ,
+};
+console.log(profile);
