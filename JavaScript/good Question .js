d32d87c148a4919c512bfaccf09521ccf5a3c32a
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-01-12 15:22:45 +0530

    for a given array with price of 5 items  , change items have an offer of 10% OFF on them .  change the array to store final price after applying offer .
    for a given array with price of 5 items  , change items have an offer of 10% OFF on them . change the array to store final price after applying offer .

---
+++ JavaScript/good Question .js
@@ -0,0 +1,15 @@
+//for a given array with price of 5 items  , change items have an offer of 10% OFF on them . 
+//change the array to store final price after applying offer . 
+
+let beforeDiscountPrice = [250 , 645 , 300, 900 , 50];
+let n = beforeDiscountPrice.length;
+let i = 0;
+let ans = 0;
+while(i<n){
+          ans = beforeDiscountPrice[i]/10;
+          beforeDiscountPrice[i] -= ans;
+          console.log(ans);
+          i++;
+}
+
+console.log(beforeDiscountPrice);
