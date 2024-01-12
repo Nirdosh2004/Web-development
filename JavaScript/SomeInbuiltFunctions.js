5bcdf2e313c2297333f19266e8fdee916227abe9
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-01-12 14:32:43 +0530

    Create SomeInbuiltFunctions.js

---
+++ JavaScript/SomeInbuiltFunctions.js
@@ -0,0 +1,24 @@
+let str = "Nirdosh Kushwaha ";
+
+console.log(str.toUpperCase());
+
+console.log(str.toLowerCase());
+console.log(str.trim());
+
+console.log(str.slice(1 )); //print from index 1 to last 
+console.log(str.slice(1 , 6 )); // print from 1 to 6
+
+let str2 = "Rishabh kushwaha";
+
+console.log(str.concat(str2));//do string ko add krne ke liye 
+
+console.log(str + str2);
+
+let search = str2.replace("h" , "y"); // for replacing word 
+console.log(search);
+
+console.log(str2.charAt(2)); // for printing character at index 
+
+
+
+
