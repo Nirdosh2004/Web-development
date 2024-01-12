d8bd1a8d6a7f0009b3eaeaccb409f7c450884743
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-01-12 14:02:36 +0530

    Create guessGameNumber.js

---
+++ JavaScript/guessGameNumber.js
@@ -0,0 +1,9 @@
+//game of guessing correct number 
+
+let correctNumber = 17;
+let userNumber = prompt("Enter the correct Number between 15 to 20 : ");
+
+while(userNumber != correctNumber){
+          userNumber  = prompt("Try again !!")
+}
+console.log("Congratulation ");
