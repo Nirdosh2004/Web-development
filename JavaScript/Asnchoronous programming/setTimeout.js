b6c41a2a45b2e25804ea164c0ba114a1b8313e31
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-01-20 09:38:13 +0530

    Create setTimeout.js

---
+++ JavaScript/Asnchoronous programming/setTimeout.js
@@ -0,0 +1,15 @@
+setTimeout( () => {
+          console.log("hello");
+} , 2000);
+
+setTimeout( () => {
+          console.log("after 4sec");
+} , 4000); //timeout 
+
+setTimeout ( () => {
+          console.log("after 6 sec");
+} , 6000);
+
+setTimeout( () => {
+          console.log("after 2 sec");
+} , 2000);
