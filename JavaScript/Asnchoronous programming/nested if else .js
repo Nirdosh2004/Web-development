65ef2a8470cb6b323216418695d64dc7c6e93049
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-01-20 09:51:19 +0530

    Create nested if else .js

---
+++ JavaScript/Asnchoronous programming/nested if else .js
@@ -0,0 +1,14 @@
+//nested if else
+
+let age = 91;
+
+if(age >= 18){
+          if(age >=60){
+                    console.log("retired");
+          }
+          else{
+                    console.log("adult");
+          }
+} else {
+          console.log("child");
+}
